07b62cdd86baeb5d09276d3a0ada203049492b7c
miladte <miladtabatabaei13857@gmail.com>
2024-04-27 01:29:21 +0430

    volvo

---
+++ js/js.js
@@ -0,0 +1,251 @@
+var languages = {
+    en:{
+        volvo:"vovlo",
+         n1:"All Recharge models",
+         n2:"volvo em90",
+         n3:"MPV",
+         n4:"$820.000",
+         n5:"Em90 fully electric",
+         n6:"Volvo Ex30",
+         n7:"SUV",
+         n8:"$515.86",
+         n9:"Ex30 fully electric",
+         n10:"Volvo Ex90",
+
+         n11:"SUV",
+         n12:"$380.56",
+         n13:"Ex90 fully electric",
+         n14:"Volvo Xc40,",
+         n15:"CUV",
+         n16:"$260.50",
+         n17:"Xc40 fully electric",
+         n18:"Why Volvo?",
+         n19:"Making your life easier",
+         n20:"Our new take on getting from A to B. Get access to a Volvo when you want it, how you want it.",
+
+         n21:"Our future is electric",
+         n22:"All our cars are available with electrified power. We help you reduce the environmental impact of daily life",
+         n23:"Protecting what’s important",
+         n24:"We’re proud of our safety heritage. We’ll keep innovating new ways to help you protect what's important.",
+         n25:"Are you ready to electrify your ride?",
+         n26:"Here’s what you need to know to confidently go electric and find the right Volvo electric car for you.",
+         n27:"Fully electric cars",
+         n28:"Our fully electric cars offer emissions-free driving without compromising range and power.",
+         n29:"Plug-in hybrid cars",
+         n30:">Plug-in hybrids pair an electric motor and a combustion engine for worry-free driving.",
+
+         n31:"Electric charging",
+         n32:"Charging your electric car at home and on the road can be easy, fast and cost-effective.",
+         n33:"About Volvo",
+         n34:"The Volvo Group (Swedish: Volvokoncernen; legally Aktiebolaget Volvo, shortened to AB Volvo, stylized as VOLVO) is a Swedish multinational manufacturing corporation headquartered in Gothenburg. While its core activity is the production, distribution and sale of trucks, buses and construction equipment, Volvo also supplies marine and industrial drive systems and financial services. In 2016, it was the world's second-largest manufacturer of heavy-duty trucks with its subsidiary Volvo Trucks. Volvo was founded in 1927. Initially involved in the automobile industry, Volvo expanded into other manufacturing sectors throughout the twentieth century. Automobile manufacturer Volvo Cars, also based in Gothenburg, was part of AB Volvo until 1999, when it was sold to the Ford Motor Company. Since 2010 Volvo Cars has been owned by the automotive company Geely Holding Group. Both AB Volvo and Volvo Cars share theVolvo logo and cooperate in running the Volvo Museum in Gothenburg, Sweden. The corporation was first listed on the Stockholm Stock Exchange in 1935, and was on the NASDAQ indices from 1985 to 2007. Volvo is one ofSweden's largest companies by market capitalisation and revenue.",
+         n35:"history",
+         n36:"Early years and international expansion The first Volvo car, a Volvo ÖV 4, left the assembly line on 14 April 1927. The brand name Volvo was originally registered as a trademark in May 1911, with the intention to be used for a new series of SKF ball bearings. It meansI rollin Latin, conjugated fromvolver. The idea was short-lived, and SKF decided to simply use its initials as the trademark for all its bearing products. In 1924, Assar Gabrielsson, an SKF sales manager, and Gustav Larson, a KTH educated engineer, decided to start construction of a Swedish car. They intended to build cars that could withstand the rigours of the country's rough roads and cold temperatures. AB Volvo began activities on 10 August 1926. After one year of preparations involving the production of ten prototypes, the firm was ready to commence the car-manufacturing business within the SKF group. The Volvo Group itself considers it started in 1927, when the first car, a Volvo ÖV 4, rolled off the production line at the factory in Hisingen, Gothenburg. Only 280 cars were built that year. The first truck, theSeries , debuted in January 1928, as an immediate success and attracted attention outside the country. In 1930, Volvo sold 639 cars, and the export of trucks to Europe started soon after; the cars did not become well known outside Sweden until after World War II. ABVolvo was introduced at the Stockholm Stock Exchange in 1935 and SKF then decided to sell its shares in the company. By 1942, Volvo acquiredthe Swedish precision engineering company Svenska Flygmotor (later renamed as Volvo Aero).",
+         n37:"Welcome to Volvo",
+         n38:"Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.",
+         n39:"Recent posts",
+         n40:"Volvo's first car",
+
+         n41:"The first Volvo car named Jacob was built on April 14, 1927",
+         n42:"Volvo's first car",
+         n43:"The first Volvo car named Jacob was built on April 14, 1927",
+         n44:"Back to top",
+         n45:"Explore Volvo vehicles",
+         n46:"SUVs",
+         n47:"Sedans",
+         n48:"Wagons",
+         n49:"Fully electric",
+         n50:"Plug-in hybrid",
+
+         n51:"Gas (mild hybrid)",
+         n52:"Help & Support",
+         n53:"Book a service",
+         n54:"Recall information",
+         n55:"Register your Volvo electric",
+         n56:"Contact us",
+         n57:"Contact Volvo",
+         n58:"Find a retailer",
+         n59:"Book a test drive",
+         n60:"DTLA studio",
+
+         n61:"Volvo Cars App",
+         n62:"Explore the app",
+         n63:"App Store",
+         n64:"App Store",
+
+
+
+    },
+    fa:{
+        volvo:"ولوو",
+         n1:"تمامی مدل های شارژی",
+         n2:"ولوو ای ام 90",
+         n3:"ام پی وی ",
+         n4:"53.300.000.000  هزار تومان",
+         n5:"ای ام 90 کاملا برقی",
+         n6:"ولوو ای اکس 30",
+         n7:"اس یو وی ",
+         n8:"33.530.900   هزار تئمان",
+         n9:"ای ام 90 کاملا برقی",
+         n10:"ولوو ای اکس 90",
+
+         n11:"اس یو وی",
+         n12:"24.736.400  هزار تومان",
+         n13:"ای ام 90 کاملا برقی",
+         n14:"ایکس سی 40",
+         n15:'اس یو وی ',
+         n16:"16.932.500  هزار تومان",
+         n17:" ایکس سی 40 کاملا برقی",
+         n18:"چرا ولوو؟",
+         n19:"زندگی شما را آسان تر می کند",
+         n20:"برداشت جدید ما در مورد رسیدن از A به B. هر زمان که می خواهید و هر طور که می خواهید به ولوو دسترسی پیدا کنید.",
+
+         n21:"آینده ما برق است",
+         n22:"تمام خودروهای ما با نیروی برقی در دسترس هستند. ما به شما کمک می کنیم اثرات زیست محیطی زندگی روزمره را کاهش دهید",
+         n23:"محافظت از آنچه مهم است",
+         n24:"ما به میراث ایمنی خود افتخار می کنیم. ما به ابداع روش‌های جدید برای کمک به شما در محافظت از آنچه مهم است ادامه خواهیم داد.",
+         n25:"آیا آماده هستید تا سواری خود را برقی کنید؟",
+         n26:"در اینجا چیزی است که باید بدانید تا با اطمینان به برق بروید و ماشین الکتریکی ولوو مناسب خود را پیدا کنید.",
+         n27:"ماشین های تمام برقی",
+         n28:"خودروهای کاملاً الکتریکی ما رانندگی بدون آلایندگی را بدون به خطر انداختن برد و قدرت ارائه می‌دهند.",
+         n29:"خودروهای هیبریدی پلاگین",
+         n30:"هیبریدهای پلاگین یک موتور الکتریکی و یک موتور احتراقی را برای رانندگی بدون نگرانی جفت می کنند.",
+
+         n31:"شارژ الکتریکی",
+         n32:"شارژ ماشین الکتریکی شما در خانه و در جاده می تواند آسان، سریع و مقرون به صرفه باشد.",
+         n33:"درباره ولوو",
+         n34:"گروه ولوو (به سوئدی: Volvokoncernen؛ به طور قانونی Aktiebolaget Volvo، خلاصه شده به AB Volvo، به سبک VOLVO) یک شرکت تولیدی چندملیتی سوئدی است که دفتر مرکزی آن در گوتنبرگ قرار دارد. در حالی که فعالیت اصلی آن تولید، توزیع و فروش کامیون، اتوبوس و تجهیزات ساختمانی است، ولوو همچنین سیستم‌های محرک دریایی و صنعتی و خدمات مالی را تامین می‌کند. در سال 2016، با شرکت تابعه ولوو تراکس، دومین تولیدکننده بزرگ کامیون‌های سنگین در جهان بود. ولوو در سال 1927 تاسیس شد. ولوو که در ابتدا در صنعت خودرو فعالیت داشت، در طول قرن بیستم به سایر بخش‌های تولیدی گسترش یافت. خودروساز ولوو کارز، که همچنین در گوتنبرگ مستقر است، تا سال 1999 بخشی از AB Volvo بود، زمانی که به شرکت خودروسازی فورد فروخته شد. از سال 2010 ولوو کارز تحت مالکیت شرکت خودروسازی جیلی هلدینگ گروپ است. هر دو AB Volvo و Volvo Cars دارای آرم Volvo مشترک هستند و در راه اندازی موزه Volvo در گوتنبرگ، سوئد همکاری می کنند. این شرکت اولین بار در سال 1935 در بورس استکهلم فهرست شد و از سال 1985 تا 2007 در شاخص های NASDAQ قرار داشت. ولوو یکی از بزرگترین شرکت های سوئد از نظر ارزش بازار و درآمد است.",
+         n35:"تاریخچه",
+         n36:"سالهای اولیه و گسترش بین المللی اولین خودروی ولوو، ولوو ÖV 4، خط مونتاژ را در 14 آوریل 1927 ترک کرد. نام تجاری ولوو در ابتدا به عنوان یک علامت تجاری در می 1911 ثبت شد، با این هدف که برای سری جدید SKF استفاده شود. بلبرینگ. در لاتین به معنای غلت می کنم است که از volvere ترکیب شده است. این ایده کوتاه مدت بود و SKF تصمیم گرفت به سادگی از حروف اول آن به عنوان علامت تجاری برای همه محصولات خود استفاده کند. در سال 1924، عصار گابریلسون، مدیر فروش SKF، و گوستاو لارسون، مهندس تحصیل کرده KTH، تصمیم گرفتند ساخت یک ماشین سوئدی را آغاز کنند. آنها قصد داشتند خودروهایی بسازند که بتوانند در برابر سختی جاده های ناهموار کشور و دمای سرد مقاومت کنند. AB Volvo فعالیت خود را در 10 اوت 1926 آغاز کرد. پس از یک سال آماده سازی شامل تولید ده نمونه اولیه، شرکت آماده بود تا تجارت خودروسازی را در گروه SKF آغاز کند. خود گروه ولوو معتقد است که این کار در سال 1927 آغاز شد، زمانی که اولین خودرو، ولوو ÖV 4، از خط تولید کارخانه در هیزینگن، گوتنبرگ خارج شد. در آن سال تنها 280 خودرو ساخته شد. اولین کامیون، سری 1، در ژانویه 1928، به عنوان یک موفقیت فوری و توجه خارج از کشور را به خود جلب کرد. در سال 1930، ولوو 639 خودرو فروخت و صادرات کامیون به اروپا بلافاصله پس از آن آغاز شد. این خودروها تا پس از جنگ جهانی دوم در خارج از سوئد به خوبی شناخته نشدند. ABVolvo در سال 1935 در بورس اوراق بهادار استکهلم معرفی شد و SKF سپس تصمیم به فروش سهام خود در این شرکت گرفت. در سال 1942، ولوو شرکت مهندسی دقیق سوئدی Svenska Flygmotor (که بعداً به Volvo Aero تغییر نام داد) را تصاحب کرد.",
+         n37:"خوش امدید به ولوو",
+         n38:"اوه، بله، شما با موفقیت این پیام هشدار مهم را خواندید. این متن نمونه قرار است کمی طولانی‌تر اجرا شود تا بتوانید ببینید که فاصله‌گذاری در یک هشدار با این نوع محتوا چگونه کار می‌کند.",
+         n39:"پستهای اخیر",
+         n40:"اولین ماشین ولوو",
+
+         n41:"اولین خودروی ولوو به نام جیکوب در 14 آوریل 1927 ساخته شد",
+         n42:"اولین ماشین ولوو",
+         n43:"اولین خودروی ولوو به نام جیکوب در 14 آوریل 1927 ساخته شد",
+         n44:"بازگشت به بالا",
+         n45:"ماشین های ولوو",
+         n46:"اس یو‌وی",
+         n47:"صندوق‌دار",
+         n48:"واگن ها",
+         n49:"کاملابرقی",
+         n50:"هیبریدی",
+
+         n51:"هیبریدملایم",
+         n52:"راهنمایی‌و‌پشتیبانی",
+         n53:"رزرو سرویس",
+         n54:"یاداوری‌اطلاعات",
+         n55:"ماشین‌خودراثبت‌کنید",
+         n56:"تماس با ما ",
+         n57:"تماس با ولوو",
+         n58:"فروشنده پیداکن",
+         n59:"تست‌رانندگی",
+         n60:"استودیو",
+
+         n61:"اپلیکیشن‌ ولوو",
+         n62:"پیداکرد‌ن‌برنامه",
+         n63:"اپ استور",
+         n64:"گوگل پلی",
+
+
+         
+
+
+
+
+    }
+}
+
+if(window.location.hash){
+    if(window.location.hash === "#fa"){
+
+          volvo.textContent = languages.fa.volvo;
+           n1.textContent = languages.fa.n1;
+           n2.textContent = languages.fa.n2;
+           n3.textContent = languages.fa.n3;
+           n4.textContent = languages.fa.n4;
+           n5.textContent = languages.fa.n5;
+           n6.textContent = languages.fa.n6;
+           n7.textContent = languages.fa.n7;
+           n8.textContent = languages.fa.n8;
+           n9.textContent = languages.fa.n9;
+           n10.textContent = languages.fa.n10;
+
+           n11.textContent = languages.fa.n11;
+           n12.textContent = languages.fa.n12;
+           n13.textContent = languages.fa.n13;
+           n14.textContent = languages.fa.n14;
+           n15.textContent = languages.fa.n15;
+           n16.textContent = languages.fa.n16;
+           n17.textContent = languages.fa.n17;
+           n18.textContent = languages.fa.n18;
+           n19.textContent = languages.fa.n19;
+           n20.textContent = languages.fa.n20;
+
+           n21.textContent = languages.fa.n21;
+           n22.textContent = languages.fa.n22;
+           n23.textContent = languages.fa.n23;
+           n24.textContent = languages.fa.n24;
+           n25.textContent = languages.fa.n25;
+           n26.textContent = languages.fa.n26;
+           n27.textContent = languages.fa.n27;
+           n28.textContent = languages.fa.n28;
+           n29.textContent = languages.fa.n29;
+           n30.textContent = languages.fa.n30;
+
+           n31.textContent = languages.fa.n31;
+           n32.textContent = languages.fa.n32;
+           n33.textContent = languages.fa.n33;
+           n34.textContent = languages.fa.n34;
+           n35.textContent = languages.fa.n35;
+           n36.textContent = languages.fa.n36;
+           n37.textContent = languages.fa.n37;
+           n38.textContent = languages.fa.n38;
+           n39.textContent = languages.fa.n39;
+           n40.textContent = languages.fa.n40;
+
+           n41.textContent = languages.fa.n41;
+           n42.textContent = languages.fa.n42;
+           n43.textContent = languages.fa.n43;
+           n44.textContent = languages.fa.n44;
+           n45.textContent = languages.fa.n45;
+           n46.textContent = languages.fa.n46;
+           n47.textContent = languages.fa.n47;
+           n48.textContent = languages.fa.n48;
+           n49.textContent = languages.fa.n49;
+           n50.textContent = languages.fa.n50;
+           n51.textContent = languages.fa.n51;
+           n52.textContent = languages.fa.n52;
+           n53.textContent = languages.fa.n53;
+           n54.textContent = languages.fa.n54;
+           n55.textContent = languages.fa.n55;
+           n56.textContent = languages.fa.n56;
+           n57.textContent = languages.fa.n57;
+           n58.textContent = languages.fa.n58;
+           n59.textContent = languages.fa.n59;
+           n60.textContent = languages.fa.n60;
+
+           n61.textContent = languages.fa.n61;
+           n62.textContent = languages.fa.n62;
+           n63.textContent = languages.fa.n63;
+           n64.textContent = languages.fa.n64;
+
+
+        //   document.body.style.fontSize = "2rem";
+        //   document.body.style.direction = "ltr";
+          document.body.style.direction="rtl";
+          document.body.style.fontSize="2rem";
+          document.body.style.alignItems="start"
+
+    }
+}
+
+var reload = document.querySelectorAll('.reload');
+
+for( i = 0 ; i <= reload.length ; i++){
+    reload[i].onclick = function(){
+        window.location.hash = this.getAttribute('href');
+        window.location.reload();
+    }
+}
